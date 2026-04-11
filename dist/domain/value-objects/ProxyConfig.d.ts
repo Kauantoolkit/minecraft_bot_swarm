708be7c7d2245b621294a1474cae6212560b59a5
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2026-04-11 04:04:07 -0300

    dist commitado é sacanagem

--- dist/domain/value-objects/ProxyConfig.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ProxyConfig {
-    readonly url: string;
-}
-export declare function parseProxyLine(line: string): ProxyConfig | null;
-//# sourceMappingURL=ProxyConfig.d.ts.map
--- dist/worker/BotWorker.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=BotWorker.d.ts.map
